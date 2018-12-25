62e07f1421fcf1b5a096b3340113aebbfc44261f
JojiKoike <joji.koike@gmail.com>
2018-12-25 12:52:04 +0900

    Find max value in list with recursive method

---
+++ chapter4/findmax.py
@@ -0,0 +1,8 @@
+def find_max(list):
+    if len(list) == 2:
+        return list[0] if list[0] > list[1] else list[1]
+    else:
+        sub_max = max(list[1:])
+        return list[0] if list[0] > sub_max else sub_max
+
+print(find_max([3,4,2,1,5]))
