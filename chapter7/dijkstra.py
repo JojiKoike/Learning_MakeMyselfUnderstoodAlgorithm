fc4e1b30e6b67e71d13ca409fe4228cf072a1ecc
JojiKoike <joji.koike@gmail.com>
2018-12-28 12:57:06 +0900

    Dijkstra Sample

---
+++ chapter7/dijkstra.py
@@ -0,0 +1,45 @@
+graph = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+graph["a"]["finish"] = 1
+graph["b"]["a"] = 3
+graph["b"]["finish"] = 5
+graph["finish"] = {}
+
+costs = {}
+infinity = float("inf")
+costs["start"] = 0
+costs["a"] = 6
+costs["b"] = 2
+costs["finish"] = infinity
+
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["finish"] = None
+
+processed = []
+
+
+def find_lowest_cost_node(costs):
+    lowest_cost = infinity
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if lowest_cost > cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
